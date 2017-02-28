b3b2b156202a242fce5d6608870448a3ce3f389f
taohonggou <cl889521@163.com>
2017-02-28 10:55:44 +0800

    刚刚新建了项目

---
+++ LearnCodeFirst.Begin/Entities/Donator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCodeFirst.Begin.Entities
+{
+    public class Donator
+    {
+        public int DonatorId { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DonateDate { get; set; }
+    }
+}
